3ec31101e565014b311298629a5ad0f43180c33e
Shinit Shetty <65714335+blackhawk005@users.noreply.github.com>
2022-01-21 01:32:24 +0530

    changed database for debug=false

--- notes/settings.py
+++ notes/settings.py
@@ -89,9 +89,12 @@ if DEBUG==True:
     }
 elif DEBUG==False:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'host.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get('API_DB_NAME'),
+            'USER': os.environ.get('API_DB_USER'),
+            'PASSWORD': os.environ.get('API_DB_PASS'),
+            'HOST': os.environ.get('API_DB_HOST'),
         }
     }
 
